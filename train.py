fe78cf8a7698014b0fb6f19abee8d729b4a7c370
WeiZongqi <969773002@qq.com>
2021-10-20 21:56:05 +0800

    change gpu num

--- train.py
+++ train.py
@@ -50,6 +50,9 @@ from utils.metrics import fitness
 from utils.loggers import Loggers
 from utils.callbacks import Callbacks
 
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 LOGGER = logging.getLogger(__name__)
 LOCAL_RANK = int(os.getenv('LOCAL_RANK', -1))  # https://pytorch.org/docs/stable/elastic/run.html
 RANK = int(os.getenv('RANK', -1))
